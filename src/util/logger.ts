98a999d8a65fd780072e77d9595c161b5fa5ec8a
Maksim Victorovich Fomin <fominmv2498@yandex.ru>
2023-03-19 20:59:45 +0300

    logger.ts refactoring

--- src/util/logger.ts
+++ src/util/logger.ts
@@ -6,17 +6,22 @@ const Console = winston.transports.Console
 const fmt     = winston.format
 
 export function createLogger(): Logger {
+    const production = process.env.NODE_ENV === "production"
+    const level      = production ? "info" : "debug"
+    const handlers   = [new Console()]
+    const format     = fmt.combine(
+        fmt.errors(),
+        fmt.colorize({ all: true }),
+        fmt.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
+        fmt.align(),
+        fmt.printf(entry => `[${entry.timestamp}] ${entry.level}: ${entry.message}`)
+    )
+
     return winston.createLogger({
-        level:             process.env.NODE_ENV === "production" ? "info" : "debug",
-        transports:        [new Console()],
-        exceptionHandlers: [new Console()],
-        rejectionHandlers: [new Console()],
-        format:            fmt.combine(
-            fmt.errors(),
-            fmt.colorize({ all: true }),
-            fmt.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
-            fmt.align(),
-            fmt.printf(entry => `[${entry.timestamp}] ${entry.level}: ${entry.message}`)
-        )
+        level,
+        format,
+        transports:        handlers,
+        exceptionHandlers: handlers,
+        rejectionHandlers: handlers,
     })
 }
